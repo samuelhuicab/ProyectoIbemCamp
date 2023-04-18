ba2485079575bb359e3352ffe14453394d7b6332
samuelhuicab <101757417+samuelhuicab@users.noreply.github.com>
2023-04-18 17:31:33 -0600

    Update altainscripcion.php

--- altainscripcion.php
+++ altainscripcion.php
@@ -34,5 +34,37 @@ if (isset($_POST['register'])) {
   
    }
 
+//    function creapdf($nombre,$email,$telefono){
+//     // include class
+//     require('plugins/fpdf185/fpdf.php');
+
+//     // create document
+//     $pdf = new FPDF();
+//     $pdf->AddPage();
+
+//     // config document
+//     $pdf->SetTitle('Generar archivos PDF con PHP');
+//     $pdf->SetAuthor('Kodetop');
+//     $pdf->SetCreator('FPDF Maker');
+
+//     // add title
+//     $pdf->SetFont('Arial', 'B', 24);
+//     $pdf->Cell(0, 10, 'Generar archivos PDF con PHP', 0, 1);
+//     $pdf->Ln();
+
+//     // add text
+//     $pdf->SetFont('Arial', '', 12);
+//     $pdf->MultiCell(0, 7, utf8_decode('Los archivos PDF se utilizan ampliamente en documentos y reportes que necesitan mantener el diseño y contenido (imágenes, tipos de letra, colores, etc), por ello vamos a aprender a crear archivos PDF utilizando PHP.'), 0, 1);
+//     $pdf->Ln();
+//     $pdf->MultiCell(0, 7, utf8_decode('FPDF es una clase PHP que permite la generación de archivos PDF de forma sencilla y sin necesidad de instalar librerías adicionales, cuenta con métodos bien documentados que facilitan su uso.'), 0, 1);
+//     $pdf->Ln();
+//     $pdf->MultiCell(0, 7, utf8_decode('Antes de comenzar lo primero es descargar FPDF e incluir los archivos necesarios en nuestro proyecto.'), 0, 1);
+//     $pdf->Ln();
+
+
+//     // output file
+//     $pdf->Output('', 'fpdf-complete.pdf');
+//    }
+
 
 ?>
