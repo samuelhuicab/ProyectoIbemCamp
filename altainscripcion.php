fced46764029c75682792200a4e66dae4b013832
samuelhuicab <101757417+samuelhuicab@users.noreply.github.com>
2023-04-18 17:11:05 -0600

    Update altainscripcion.php

--- altainscripcion.php
+++ altainscripcion.php
@@ -4,21 +4,30 @@ if (isset($_POST['register'])) {
     $nombre = $_POST['nomb'];
     $email = $_POST['email'];
     $telefono = $_POST['tel'];
-
-    echo $nombre;
-    echo $email;
-    echo $telefono;
+    $status = "A";
   
-    // try {
-    //   require_once('administracion/include/funciones/bd_conexion.php');
-    //   $stmt = $conn->prepare("");
-    //   $stmt->bind_param("ssss", $nombre, $usuario, $password_hashed, $status);
-    //   $stmt->execute();
-    // } catch (\Exception $e) {
-    //   echo "error ".$e->getMessage();
-    // }
+    try {
+        require_once('administracion/include/funciones/bd_conexion.php');
+        $stmt = $conn->prepare("INSERT INTO usuariopreinscritos (nombre,email,telefono,estatus) VALUES (?, ?, ?, ?)");
+        $stmt->bind_param("ssss", $nombre, $email, $telefono, $status);
+        $stmt->execute();
+        $id_registro = $stmt->insert_id;
+        if ($id_registro > 0) {
+          $respuesta = array(
+            'respuesta' => 'exito',
+          );
+        }else {
+          $respuesta = array(
+            'respuesta' => 'error',
+          );
+        }
+        $stmt->close();
+        $conn->close();
+      } catch (\Exception $e) {
+        echo "error ".$e->getMessage();
+      }
   
-    // die(json_encode($respuesta));
+    die(json_encode($respuesta));
   
   
   
