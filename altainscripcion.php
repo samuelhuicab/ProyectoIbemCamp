8f0103df8bc54c591df2d189b7fa6b95aa8e8990
samuelhuicab <101757417+samuelhuicab@users.noreply.github.com>
2023-04-18 15:17:44 -0600

    Update altainscripcion.php

--- altainscripcion.php
+++ altainscripcion.php
@@ -1,5 +1,29 @@
 <?php 
+if (isset($_POST['register'])) {
 
+    $nombre = $_POST['nomb'];
+    $email = $_POST['email'];
+    $telefono = $_POST['tel'];
+
+    echo $nombre;
+    echo $email;
+    echo $telefono;
+  
+    // try {
+    //   require_once('administracion/include/funciones/bd_conexion.php');
+    //   $stmt = $conn->prepare("");
+    //   $stmt->bind_param("ssss", $nombre, $usuario, $password_hashed, $status);
+    //   $stmt->execute();
+    // } catch (\Exception $e) {
+    //   echo "error ".$e->getMessage();
+    // }
+  
+    // die(json_encode($respuesta));
+  
+  
+  
+  
+   }
 
 
 ?>
