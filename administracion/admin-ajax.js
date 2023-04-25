9af041b496e6058f92724aee1016c93665165ff9
samuelhuicab <101757417+samuelhuicab@users.noreply.github.com>
2023-04-25 14:20:06 -0600

    Update admin-ajax.js

--- administracion/admin-ajax.js
+++ administracion/admin-ajax.js
@@ -41,4 +41,41 @@ $(document).ready(function() {
     })
   });
 
+  $('#login-campers').on('submit', function(e) {
+
+    e.preventDefault();
+
+    var datos = $(this).serializeArray();
+
+    $.ajax({
+      type: $(this).attr('method'),
+      data: datos,
+      url: $(this).attr('action'),
+      dataType: 'json',
+      success: function(data) {
+        var resultado = data;
+        if (resultado.respuesta == 'exito') {
+          setTimeout(function(){
+            window.location.href = '../cuestionario/index.php';
+          }, 1000);
+        }else if (resultado.respuesta == 'TokenVencido') {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Tiene Un token Invalido, Intenta Reinscribiendote de nuevo con el mismo correo.',
+            })
+        }else {
+          Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Datos incorrectos de sesión',
+                    })
+        }
+      }
+
+
+
+    })
+  });
+
 });
