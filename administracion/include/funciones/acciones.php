ac16f9f32e69bd2324a635055de0329802715252
samuelhuicab <101757417+samuelhuicab@users.noreply.github.com>
2023-04-25 18:42:59 -0600

    Update acciones.php

--- administracion/include/funciones/acciones.php
+++ administracion/include/funciones/acciones.php
@@ -2,64 +2,63 @@
 
 if (isset($_POST['login-campers-b'])) {
 
-    $password = $_POST['pwd'];
-    $correo = $_POST['correo'];
-    $fecha_actual = date("Y-m-d H:i:s");
+  $password = $_POST['pwd'];
+  $correo = $_POST['correo'];
+  $fecha_actual = date("Y-m-d H:i:s");
 
-    try {
-      require_once('bd_conexion.php');
-      $sql = "SELECT diasCaducidad FROM configuracioncaducidadtoken";
-      $caducidadDias = $conn->query($sql);
-      $resultado = $caducidadDias->fetch_assoc();
-      $diacaducas = $resultado['diasCaducidad'];
-      $stmt = $conn->prepare("SELECT usuarioPreInscritoID, nombre, email, iglesiaPerteneciente,token,fechaTokenCreacion FROM usuariopreinscritos WHERE email = ?;");
-      $stmt->bind_param("s", $correo);
-      $stmt->execute();
-      $stmt->bind_result($IDusuarioPre, $nombreusuarioPre, $emailusuarioPre, $iglesiausuarioPre, $tokenusuarioPre, $fechatokenusuarioPre);
-      if ($stmt->affected_rows) {
-        $existe = $stmt->fetch();
-        if ($existe) {
-          if (password_verify($password, $tokenusuarioPre)) {
-            $fecha1 = DateTime::createFromFormat('Y-m-d H:i:s', $fecha_actual);
-            $fecha2 = DateTime::createFromFormat('Y-m-d H:i:s', $fechatokenusuarioPre);
-            $fecha1new = $fecha1->format("d-m-Y");
-            $fecha2new = $fecha2->format("d-m-Y");
-            $fecha1new1 = new DateTime($fecha1new);
-            $fecha2new2 = new DateTime($fecha2new);
-            $diferenciadias = $fecha1new1->diff($fecha2new2);
-            $diff = $diferenciadias->days;
-            if ($diff > $diacaducas){
-                $respuesta = array('respuesta' => 'TokenVencido' );
-            }else{
-                session_start();
-                $_SESSION['nombre'] = $nombreusuarioPre;
-                $_SESSION['correo'] = $emailusuarioPre;
-                $respuesta = array('respuesta' => 'exito' );
-            }
-          }else {
-            $respuesta = array('respuesta' => 'password_incorrecto');
+  try {
+    require_once('bd_conexion.php');
+    $sql = "SELECT diasCaducidad FROM configuracioncaducidadtoken";
+    $caducidadDias = $conn->query($sql);
+    $resultado = $caducidadDias->fetch_assoc();
+    $diacaducas = $resultado['diasCaducidad'];
+    $stmt = $conn->prepare("SELECT usuarioPreInscritoID, nombre, email, iglesiaPerteneciente,token,fechaTokenCreacion FROM usuariopreinscritos WHERE email = ?;");
+    $stmt->bind_param("s", $correo);
+    $stmt->execute();
+    $stmt->bind_result($IDusuarioPre, $nombreusuarioPre, $emailusuarioPre, $iglesiausuarioPre, $tokenusuarioPre, $fechatokenusuarioPre);
+    if ($stmt->affected_rows) {
+      $existe = $stmt->fetch();
+      if ($existe) {
+        if (password_verify($password, $tokenusuarioPre)) {
+          $fecha1 = DateTime::createFromFormat('Y-m-d H:i:s', $fecha_actual);
+          $fecha2 = DateTime::createFromFormat('Y-m-d H:i:s', $fechatokenusuarioPre);
+          $fecha1new = $fecha1->format("d-m-Y");
+          $fecha2new = $fecha2->format("d-m-Y");
+          $fecha1new1 = new DateTime($fecha1new);
+          $fecha2new2 = new DateTime($fecha2new);
+          $diferenciadias = $fecha1new1->diff($fecha2new2);
+          $diff = $diferenciadias->days;
+          if ($diff > $diacaducas){
+              $respuesta = array('respuesta' => 'TokenVencido' );
+          }else{
+              session_start();
+              $_SESSION['nombre'] = $nombreusuarioPre;
+              $_SESSION['correo'] = $emailusuarioPre;
+              $_SESSION['IDusuarioPre'] = $IDusuarioPre;
+              $respuesta = array('respuesta' => 'exito' );
           }
         }else {
-          $respuesta = array(
-            'respuesta' => 'no_existe',
-          );
+          $respuesta = array('respuesta' => 'password_incorrecto');
         }
+      }else {
+        $respuesta = array(
+          'respuesta' => 'no_existe',
+        );
       }
-      $stmt->close();
-      $conn->close();
-    } catch (\Exception $e) {
-      echo "error ". $e->getMessage();
     }
-  
-  
-      die(json_encode($respuesta));
-  
-  
-  
-  
-  
-   }
-  
+    $stmt->close();
+    $conn->close();
+  } catch (\Exception $e) {
+    echo "error ". $e->getMessage();
+  }
 
+
+    die(json_encode($respuesta));
+
+
+
+
+
+ }
 
 ?>
